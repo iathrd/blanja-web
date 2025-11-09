a079caa81ac2fc0d50c40a24fa6ac261697c5f7a
Iqbal Athorid <iqbalathoridd@gmail.com>
2025-11-09 16:51:14 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -34,6 +35,7 @@ export default function Home() {
             center.
           </p>
         </div>
+        <Button>HAHAH</Button>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactPlugin from "eslint-plugin-react";
+import prettier from "eslint-config-prettier/flat";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      "react/jsx-uses-react": "off", // Not needed with Next.js + React 17+
+      "react/react-in-jsx-scope": "off", // Also off for Next.js
+      "react/prop-types": "off", // Common for TypeScript projects
+      // Add more React rules if needed
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
